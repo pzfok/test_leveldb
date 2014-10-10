b425d633181f79bb61ebc1c5f1abeb164fb5132f
taurzk <taurzk@gmail.com>
2014-10-10 12:06:49 +0800

    first version

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ leveldbdemo
 ===========
 
 learn leveldb
+purpose:
+1. learn levelDB.
+2. test the parameters affect.
+3. stress testing: input large amount data to DB, key is value's sha1, value is 26 len alphabet characters string, and can be repeated.
---
+++ demo.cc
@@ -0,0 +1,32 @@
+#include "leveldb/db.h"
+#include <iostream>
+ 
+using namespace std;
+ 
+int main()
+{
+  leveldb::DB *db;
+  leveldb::Options options;
+ 
+  options.create_if_missing = false;
+ 
+  // 開啟數據庫
+  leveldb::DB::Open(options, "./testdb", &db);
+ 
+  // 鍵 = MyKey29，值 = "Hello World!"
+  string key = "MyKey29", value = "Hello World!", result;
+ 
+  // 儲存 鍵／值對
+  db->Put(leveldb::WriteOptions(), key, value);
+ 
+  // 查詢 MyKey29 鍵的值
+  db->Get(leveldb::ReadOptions(), key, &result);
+ 
+  // 輸出值到屏幕
+  cout << "result = " << result << endl;
+ 
+  // 關閉數據庫
+  delete db;
+ 
+  return 0;
+}
