a081e52e80167494bcc3432fffc6eeb2f7f0cf79
taurzk <taurzk@gmail.com>
2014-10-14 16:10:21 +0800

    update readme

--- README.md
+++ README.md
@@ -5,4 +5,7 @@ learn leveldb
 purpose:    
 1. learn levelDB.  
 2. test the parameters affect.  
-3. stress testing: input large amount data to DB, key is value's sha1, value is 26 len alphabet characters string, and can be repeated.    
+3. stress testing: input large amount data to DB, key is value's sha1, value is string which consisting of 26 characters, and can be repeated.    
+    - Read and write single-threaded
+    - Multithreading
+    - threadingpool
--- demo.cc
+++ demo.cc
@@ -1,17 +1,25 @@
-#include "leveldb/db.h"
 #include <iostream>
+#include "leveldb/db.h"
  
 using namespace std;
  
+
 int main()
 {
   leveldb::DB *db;
   leveldb::Options options;
  
   options.create_if_missing = true;
+  //options.error_if_exists = true;
  
   // 開啟數據庫
-  leveldb::DB::Open(options, "./testdb", &db);
+  leveldb::Status s = leveldb::DB::Open(options, "./testdb", &db);
+
+  if (!s.ok())
+  {
+      cerr << s.ToString() << endl;
+      return 1;
+  }
  
   // 鍵 = MyKey29，值 = "Hello World!"
   string key = "MyKey29", value = "Hello World!", result;
@@ -24,9 +32,16 @@ int main()
  
   // 輸出值到屏幕
   cout << "result = " << result << endl;
- 
+
+  cout << "print all key" << endl;
+  leveldb::Iterator* it = db->NewIterator(leveldb::ReadOptions());
+  for (it->SeekToFirst(); it->Valid(); it->Next()) {
+      cout << it->key().ToString() << " : "  << it->value().ToString() << endl;
+  }
+  delete it;
+
   // 關閉數據庫
   delete db;
- 
+
   return 0;
 }
